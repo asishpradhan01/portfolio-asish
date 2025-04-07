caaa8f64b2f8b24841b179f839957eecdcc5d0e1
asishpradhan01 <153635662+asishpradhan01@users.noreply.github.com>
2025-04-07 12:42:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".hamburger-ulelements")
+    const icons = document.querySelector(".hamburger-icons")
+    menu.classList.toggle("open")
+    icons.classList.toggle("open")
+}                                      
